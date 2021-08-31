08494a1b3ac3ad914efb1e5c888085ea99e7c96b
Richie Casto <rcasto92@gmail.com>
2021-08-31 19:09:11 -0400

    first complete set of unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -31,16 +31,112 @@ it('can handle delete edit', () => {
     expect(result).toEqual(expectedResult);
 });
 
-// console.log(editDistance('foo', 'food'));
+it('can handle add edit', () => {
+    const expectedResult: IEditDistanceResult = {
+        distance: 1,
+        records: [{
+            type: EDIT_RECORD_TYPE.ADD,
+            str1: {
+                index: -1,
+                value: '',
+            },
+            str2: {
+                index: 0,
+                value: 'a',
+            },
+        }],
+    };
+    const result = editDistance('', 'a');
 
-// console.log(editDistance('', 'a'));
-// console.log(editDistance('a', 'b'));
-// console.log(editDistance('a', 'a'));
+    expect(result).toEqual(expectedResult);
+});
 
-// console.log(editDistance('ab', 'a'));
-// console.log(editDistance('ab', 'abc'));
-// console.log(editDistance('ab', 'cb'));
-// console.log(editDistance('ab', 'ab'));
+it('can handle replace edit', () => {
+    const expectedResult: IEditDistanceResult = {
+        distance: 1,
+        records: [{
+            type: EDIT_RECORD_TYPE.REPLACE,
+            str1: {
+                index: 0,
+                value: 'a',
+            },
+            str2: {
+                index: 0,
+                value: 'b',
+            },
+        }],
+    };
+    const result = editDistance('a', 'b');
 
-// console.log(editDistance('browser', 'browse'));
-// console.log(editDistance('GCTATGCCACGC', 'GCGTATGCACGC'));
+    expect(result).toEqual(expectedResult);
+});
+
+it('can handle match edit', () => {
+    const expectedResult: IEditDistanceResult = {
+        distance: 0,
+        records: [{
+            type: EDIT_RECORD_TYPE.MATCH,
+            str1: {
+                index: 0,
+                value: 'a',
+            },
+            str2: {
+                index: 0,
+                value: 'a',
+            },
+        }],
+    };
+    const result = editDistance('a', 'a');
+
+    expect(result).toEqual(expectedResult);
+});
+
+it('can handle multiple edits together', () => {
+    const expectedResult: IEditDistanceResult = {
+        distance: 3,
+        records: [{
+            type: EDIT_RECORD_TYPE.MATCH,
+            str1: {
+                index: 0,
+                value: 'a',
+            },
+            str2: {
+                index: 0,
+                value: 'a',
+            },
+        }, {
+            type: EDIT_RECORD_TYPE.ADD,
+            str1: {
+                index: 0,
+                value: 'a',
+            },
+            str2: {
+                index: 1,
+                value: 'b',
+            },
+        }, {
+            type: EDIT_RECORD_TYPE.ADD,
+            str1: {
+                index: 0,
+                value: 'a',
+            },
+            str2: {
+                index: 2,
+                value: 'c',
+            },
+        }, {
+            type: EDIT_RECORD_TYPE.REPLACE,
+            str1: {
+                index: 1,
+                value: 'e',
+            },
+            str2: {
+                index: 3,
+                value: 'd',
+            },
+        }],
+    };
+    const result = editDistance('ae', 'abcd');
+
+    expect(result).toEqual(expectedResult);
+});
