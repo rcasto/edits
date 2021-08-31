7f33ab5e62805b632224c19a5d0a776dc069e704
Richie Casto <rcasto92@gmail.com>
2021-08-31 18:23:08 -0400

    stash new lib progress/port over

---
+++ src/index.test.ts
@@ -0,0 +1,15 @@
+// console.log(editDistance('foo', 'food'));
+// console.log(editDistance('', ''));
+
+// console.log(editDistance('a', ''));
+// console.log(editDistance('', 'a'));
+// console.log(editDistance('a', 'b'));
+// console.log(editDistance('a', 'a'));
+
+// console.log(editDistance('ab', 'a'));
+// console.log(editDistance('ab', 'abc'));
+// console.log(editDistance('ab', 'cb'));
+// console.log(editDistance('ab', 'ab'));
+
+// console.log(editDistance('browser', 'browse'));
+// console.log(editDistance('GCTATGCCACGC', 'GCGTATGCACGC'));
